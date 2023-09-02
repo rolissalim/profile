e90c13bdf1eef56c4488b10722f94b601edf83f9
nurdin <nurdin@bdg.ebdesk.com>
2023-09-02 15:04:31 +0700

    add env prod folder in src modify webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,15 @@
 const path = require('path')
+import { ProvidePlugin } from 'webpack';
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
-module.exports = {
+interface Configuration extends WebpackConfiguration {
+    devServer?: WebpackDevServerConfiguration;
+}
+
+import ESLintPlugin from 'eslint-webpack-plugin';
+
+const config: Configuration = {
     entry: './src/index.tsx',
     output: {
         publicPath: '/',
@@ -9,4 +18,51 @@ module.exports = {
     //     path: path.resolve(__dirname, 'build'),
     //     filename: 'main-[hash].js'
     // }
-}
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // require.resolve('babel-loader')
+                    options: {
+                        plugins: [process.env.NODE_ENV == 'development' && require.resolve('react-refresh/babel')].filter(Boolean),
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                    },
+                },
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        alias: {
+            '@app': path.resolve(__dirname, 'src/app/'),
+            '@configs': path.resolve(__dirname, 'src/app/configs/'),
+            '@helpers': path.resolve(__dirname, 'src/app/helpers/'),
+            '@interfaces': path.resolve(__dirname, 'src/app/interfaces/'),
+            '@components': path.resolve(__dirname, 'src/app/components/'),
+            '@modules': path.resolve(__dirname, 'src/app/modules/'),
+            '@pages': path.resolve(__dirname, 'src/app/pages/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+        },
+        fallback: {
+            os: false,
+        },
+    },
+    plugins: [
+        new ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        }),
+        new ESLintPlugin({
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+        }),
+    ],
+};
+
+export default config;
